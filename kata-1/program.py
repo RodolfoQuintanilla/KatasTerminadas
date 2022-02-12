08d5ba727e3bedcb2e4899fbc8eafd2474bc2b5a
Rodolfo <rqm@outlook.es>
2022-02-12 16:26:48 -0600

    Primeras 6 Katas

---
+++ kata-1/program.py
@@ -0,0 +1,49 @@
+### suma###
+from datetime import date
+sum = 1 + 2
+print(sum)
+
+### Imprime desde Consola###
+print('Hola desde la consola')
+
+### calculos###
+sum = 1 + 2  # 3
+product = sum * 2
+print(product)
+
+
+###Tipo de datos###
+
+# Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+type(distancia_a_alfa_centauri)
+
+###Fechas###
+# Importamos la biblioteca
+# Obtenemos la fecha de hoy
+date.today()
+
+# Mostramos la fecha en la consola
+print(date.today())
+
+###Conversiopn de tipos de datos###
+# Tu turno ejecuta el siguiente comando: print("Today's date is: " + str(date.today()))
+print("Today's date is: " + str(date.today()))
+
+
+###Rcopilar Informacion ###
+# Escribe tu código aquí
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+
+# Escribe tu códigprint("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
+
+# Escribe tu código aquí
+print(int(first_number) + int(second_number))
